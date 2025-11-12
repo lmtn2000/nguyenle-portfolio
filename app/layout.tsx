6878fc1f4130c88af9e96a6d6e6972e9e14752f1
watagumo <watagumo@Aircuawatagumo.lan>
2025-11-12 23:21:53 +0700

    deploy actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,8 @@ import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
-import { baseUrl } from './sitemap'
 
 export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
   title: {
     default: 'Next.js Portfolio Starter',
     template: '%s | Next.js Portfolio Starter',
@@ -18,7 +16,6 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'My Portfolio',
     description: 'This is my portfolio.',
-    url: baseUrl,
     siteName: 'My Portfolio',
     locale: 'en_US',
     type: 'website',
